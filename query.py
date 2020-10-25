Corrected the formula, call to main() in tfidf.py and Documentation changes

query.py
@@ -31,36 +31,36 @@ def query_processing(query):
     '''
     no_of_doc = 34886+1 # Actual number of documents + 1
 
-    #all alphabets to lower case
+    # All alphabets to lower case
     query= query.lower() 
 
-    #preprocessing query removing unnecessary characters
+    # Preprocessing query removing unnecessary characters
     query = query.replace("\n"," ").replace("\r"," ")
     query = query.replace("'s"," ")
     punctuationList = '!"#$%&\()*+,-./:;<=>?@[\\]^_{|}~'
     x = str.maketrans(dict.fromkeys(punctuationList," "))
     query = query.translate(x)
 
-    #tokenize
+    # Tokenize
     df=word_tokenize(query)
     query_length=len(df)
     df= [w for w in df if not w in stopwords.words('english') ]
     
-    #stemming
+    # Stemming
     ps = PorterStemmer() 
     df=[ps.stem(word) for word in df]
     
-    #term frequency 
+    # Term frequency 
     query_freq = defaultdict(lambda: 0)
 
     for token in df :
         query_freq[token] +=1
 
-    #calling helper functions to calculate tf-idf for query with documents and title as corpus
+    # Calling helper functions to calculate tf-idf for query with documents and title as corpus
     q_ls, q_tf_idf = helper("inverted_index.obj", "processed_data.obj", "Length", no_of_doc, query_length, query_freq)
     q_ls_title, q_tf_idf_title = helper("inverted_index_title.obj", "processed_data_title.obj", "TitleLength", no_of_doc, query_length, query_freq)
 
-    #cosine similarity
+    # Cosine similarity
     tf_idf = load("tf-idf.obj")
     tf_idf_title = load("tf-idf_title.obj")
 
@@ -70,9 +70,9 @@ def query_processing(query):
 
     rank_heap=[]
     for doc in range(0,no_of_doc-1) :
-        val=0   # dot-product
+        val=0   # Dot-product
         epsilon=10e-9
-        d_ls = [] #store tf-idf values for a doc
+        d_ls = [] # Store tf-idf values for a doc
         for term , value in tf_idf[doc].items() :
             if (term in q_tf_idf.keys()):
                 val+=(value * q_tf_idf[term])
@@ -99,7 +99,7 @@ def helper(inverted_index, processed_data, length, no_of_doc, query_length, quer
     Calculates BM25 tf-idf for query in a way similar to document.
     Takes the corpus parameters and returns tf-idf values.
     '''
-    #tf-idf for query
+    # tf-idf for query
     ii_df = load(inverted_index)
 
     ii_df= ii_df.to_dict()
@@ -120,10 +120,8 @@ def helper(inverted_index, processed_data, length, no_of_doc, query_length, quer
         tf = (value/query_length)
         if key in ii_df.keys(): 
             idf = np.log(no_of_doc/ii_df[key])
-            # q_tf_idf[key]=tf*idf
-            q_tf_idf[key] = idf*( tf*(k+1) )/(k*(1- b + b*(query_length/avg_length))) *100
-        # q_ls=q_tf_idf[key]=tf*idf
-        q_ls+= [np.log(no_of_doc)*( tf*(k+1) )/(k*(1- b + b*(query_length/avg_length))) *100] # add the value of tf idf of each term to the list
+            q_tf_idf[key] = idf * ( tf * (k+1) ) / (tf + k * (1- b + b * (query_length / avg_length))) *100
+        q_ls+= [np.log(no_of_doc) * ( tf * (k+1) ) / (tf + k * (1- b + b * (query_length / avg_length))) *100] # add the value of tf idf of each term to the list
 
     return q_ls, q_tf_idf
 

tfidf.py
@@ -30,37 +30,36 @@ def tf_idf_preprocess(processed_data, inverted_index, length):
     start_time = time.time()
     no_of_doc = 34886
 
-    # loading term frequencies
+    # Loading term frequencies
     df = load(processed_data)
 
     d_df = df.to_dict()
     d_df =d_df[length]
     
 
-    # average length of all documents
+    # Average length of all documents
     avg_length= df[length].mean() 
     
-    #loading indexing list
+    # Loading indexing list
     ii_df = load(inverted_index)# indexing list
 
     ii_df= ii_df.to_dict()
     ii_df=ii_df['PostingList'] 
 
-    k=1.75 # parameter for BM25
-    b=0.75 # parameter for BM25
+    k=1.75 # Parameter for BM25
+    b=0.75 # Parameter for BM25
     
     tf_idf_dict={}
 
-    #calculating tf-idf
+    # Calculating tf-idf
     for doc in range(0,no_of_doc):
         doc_dict={}
         for key,value in df['Frequency'][doc].items():
             if key=='nan' or key=='null':
-             continue;
+             continue
             tf = (value/d_df[doc]) 
             idf = np.log(no_of_doc/(ii_df[key]))
-            # doc_dict[key]=tf*idf
-            doc_dict[key] = idf*( tf*(k+1) )/(k*(1- b + b*(df[length][doc]/avg_length))) * 100
+            doc_dict[key] = idf*( tf*(k+1) )/(tf + k*(1- b + b*(df[length][doc]/avg_length))) * 100
         tf_idf_dict[doc]=doc_dict
 
     print("--- %s seconds ---" % (time.time() - start_time))
@@ -77,7 +76,7 @@ def main():
     pickle.dump(tf_idf_title_dict,filehandler)
     filehandler.close()
 
-
+main()
 
 
 